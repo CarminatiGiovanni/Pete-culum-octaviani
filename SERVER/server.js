01d5325fed9fb6317cfa3d36964b1c87ef1cc9e3
HeyJOe03 <jocarmi03@gmail.com>
2021-09-16 19:12:56 +0200

    changed the heroku PGURI whith ?ssl=true

--- SERVER/server.js
+++ SERVER/server.js
@@ -13,17 +13,17 @@ const data =     {
     port: process.env.PGPORT,
 }
 
-//const DB = null;
-let DB = new Client(data)
-//if(process.env.DATABASE_URL) {console.log(process.env.DATABASE_URL); DB = new Client(process.env.DATABASE_URL)}
-//else DB = new Client(data)
+let DB = null;
+//let DB = new Client(data)
+if(process.env.PGURI) {console.log(process.env.PGURI); DB = new Client(process.env.PGURI)}
+else DB = new Client(data)
 
 //console.log(DB)
 
 DB
     .connect()
     .then(() => {console.log(`PostgreSQL DB successfully connected!`); DBstate = true;})
-    .catch(err => {console.log(`Unable to connect the DB`);DBstate = false;throw err})
+    .catch(err => {console.log(`Unable to connect the DB`);DBstate = false;})
 
 
 app.get('/',(req,res) => {
