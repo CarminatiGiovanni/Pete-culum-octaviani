f45cc74f6cbe8025fd9ece032caccb4813f9a076
HeyJOe03 <jocarmi03@gmail.com>
2021-09-16 18:01:15 +0200

    Added DB pg connection

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,10 +1,20 @@
 const app = require('express')()
 require('dotenv').config()
+const {Client} = require('pg')
 
 const PORT = process.env.PORT || 3000
+let DBstate = false;
+
+const DB = new Client()
+DB
+    .connect()
+    .then(() => {console.log(`PostgreSQL DB successfully connected!`); DBstate = true;})
+    .catch(err => {console.log(`Unable to connect the DB`);DBstate = false;})
+
 
 app.get('/',(req,res) => {
-    res.send('hello world!')
+    if(DBstate)res.send('hello world!')
+    else res.send('No db connected')
 })
 
 app.listen(PORT,() => console.log(`>Server is listening on PORT: ${PORT}`))
