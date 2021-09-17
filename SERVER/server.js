c4c116f57cd23e285c85dbfdfa0776bb565e3318
HeyJOe03 <jocarmi03@gmail.com>
2021-09-17 08:19:55 +0200

    added static path

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,6 +1,8 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 require('dotenv').config()
 const {Client} = require('pg')
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
 let DBstate = false;
@@ -18,6 +20,8 @@ let DB = null;
 if(process.env.DATABASE_URL) {console.log(process.env.DATABASE_URL); DB = new Client({connectionString: process.env.DATABASE_URL,ssl:true});}
 else DB = new Client(data)
 
+app.use(express.static(path.join(__dirname, '../CLIENT')))
+
 //console.log(DB)
 
 DB
@@ -26,9 +30,15 @@ DB
     .catch(err => {console.log(`Unable to connect the DB`);DBstate = false;throw err})
 
 
-app.get('/',(req,res) => {
-    if(DBstate)res.send(`Hello world! \n ${JSON.stringify(data)}`)
-    else res.send(`No db connected \n ${JSON.stringify(data)}`)
-})
+// app.get('/',(req,res) => {
+//     //if(DBstate)res.send(`Hello world! \n ${JSON.stringify(data)}`)
+//     //else res.send(`No db connected \n ${JSON.stringify(data)}`)
+//     //const pathf = path.join(-__dirname,'../CLIENT/index.html')
+//     //console.log(pathf)
+//     //res.sendFile(pathf);
+    
+//     const pathf = (path.join(__dirname, '../CLIENT/index.html'))
+//     res.sendFile(pathf)
+// })
 
 app.listen(PORT,() => console.log(`>Server is listening on PORT: ${PORT}`))
