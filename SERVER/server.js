53e83782b22ec65fa921305f8601bf0d10ce972a
HeyJOe03 <jocarmi03@gmail.com>
2021-09-17 15:27:41 +0200

    added a simple comment, last commit bevore join branch

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
+require('dotenv').config() //FIXME: remove before final version
 const path = require('path')
 const mongoose = require('mongoose')
 
