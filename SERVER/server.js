e87c47e5d40426ed940f5ffad5a521e69a7a5ca9
HeyJOe03 <jocarmi03@gmail.com>
2021-09-17 15:20:49 +0200

    connected MONGODB

--- SERVER/server.js
+++ SERVER/server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log(err))
+
+//const connectionMDB = `mongodb+srv://HeyJOe:pco123@mondodbpco.p5ltj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const PORT = process.env.PORT || 3000
 
