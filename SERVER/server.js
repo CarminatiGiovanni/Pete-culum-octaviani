b1ccd63c0bda4ab7bbedc3744376d48990009691
HeyJOe03 <jocarmi03@gmail.com>
2021-09-17 08:49:22 +0200

    commented

--- SERVER/server.js
+++ SERVER/server.js
@@ -23,12 +23,12 @@ if(process.env.DATABASE_URL) {
     DB = new Client(
         {
             connectionString: process.env.DATABASE_URL, 
-            ssl: {
-                rejectUnauthorized: false,
-                ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
-                key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
-                cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
-            },
+            // ssl: {
+            //     rejectUnauthorized: false,
+            //     ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+            //     key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+            //     cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+            // },
         }
     );
 }
