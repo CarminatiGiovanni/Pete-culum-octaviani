3906215834a6a9ef7cce820bc80724921dbc8853
HeyJOe03 <jocarmi03@gmail.com>
2021-09-17 08:22:33 +0200

    changed ssl

--- SERVER/server.js
+++ SERVER/server.js
@@ -17,28 +17,20 @@ const data =     {
 
 let DB = null;
 //let DB = new Client(data)
-if(process.env.DATABASE_URL) {console.log(process.env.DATABASE_URL); DB = new Client({connectionString: process.env.DATABASE_URL,ssl:true});}
+if(process.env.DATABASE_URL) {console.log(process.env.DATABASE_URL); DB = new Client({connectionString: process.env.DATABASE_URL, ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+    key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+  },});}
 else DB = new Client(data)
 
 app.use(express.static(path.join(__dirname, '../CLIENT')))
-
-//console.log(DB)
 
 DB
     .connect()
     .then(() => {console.log(`PostgreSQL DB successfully connected!`); DBstate = true;})
     .catch(err => {console.log(`Unable to connect the DB`);DBstate = false;throw err})
 
-
-// app.get('/',(req,res) => {
-//     //if(DBstate)res.send(`Hello world! \n ${JSON.stringify(data)}`)
-//     //else res.send(`No db connected \n ${JSON.stringify(data)}`)
-//     //const pathf = path.join(-__dirname,'../CLIENT/index.html')
-//     //console.log(pathf)
-//     //res.sendFile(pathf);
-    
-//     const pathf = (path.join(__dirname, '../CLIENT/index.html'))
-//     res.sendFile(pathf)
-// })
 
 app.listen(PORT,() => console.log(`>Server is listening on PORT: ${PORT}`))
