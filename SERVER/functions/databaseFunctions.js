35b16906f8e564d38e7a76ea9fcab909d33a904e
HeyJOe03 <jocarmi03@gmail.com>
2021-09-17 21:37:28 +0200

    testing on the frontend

--- SERVER/functions/databaseFunctions.js
+++ SERVER/functions/databaseFunctions.js
@@ -1,30 +1,34 @@
-let insertFunction = (req,res) => {
-    //res.send(JSON.stringify(req.params))
-    // const bus = new Bus({
-    //     id:"AA123BB",
-    //     hleaves:"15:30",
-    //     day:"MON", //MON TUE WEN THU FRI SAT SUN
-    //     stops:[
-    //         {n:0,stop:'Piazza Brembana',h:'15:30'},
-    //         {n:1,stop:'San Giovanni Bianco',h:'15:45'},
-    //         {n:2,stop:'San Pellegrino',h:'16:00'},
-    //         {n:3,stop:'Zogno',h:'16:15'},
-    //         {n:4,stop:'Bergamo',h:'17:30'},
-    //     ],
-    //     passengers:{
-    //         people:10,
-    //         bags:5,
-    //         suitcases:1,
-    //         scooters:1,
-    //         prams:0,
-    //         strollers:0,
-    //         dogs:0
-    //     }
-    // })
+const Bus =                 require('./../models/bus')
 
-    // bus.save()
-    //     .then((result) => {res.send(result)})
-    //     .catch(err => console.log("error"))
+let insertFunction = (req,res) => {
+    // const bus = new Bus(
+    //     {
+    //         id:"PW236KF",
+    //         hleaves:"06:30",
+    //         day:"SAT", //MON TUE WEN THU FRI SAT SUN
+    //         stops:[
+    //             {n:0,stop:'Sorisole',h:'06:30'},
+    //             {n:1,stop:'Ponteranica',h:'06:45'},
+    //             {n:2,stop:'Valtesse',h:'07:15'},
+    //             {n:3,stop:'Monte Rosso',h:'07:25'},
+    //             {n:4,stop:'Berbenno',h:'7:45'},
+    //         ],
+    //         passengers:{
+    //             people:11,
+    //             bags:7,
+    //             suitcases:2,
+    //             scooters:0,
+    //             prams:1,
+    //             strollers:1,
+    //             dogs:2
+    //         }
+    //     }
+    //)
+
+
+    bus.save()
+        .then((result) => {res.send(result)})
+        .catch(err => console.log(err))
 }
 
 let selectAllFunction = (req,res) => {
