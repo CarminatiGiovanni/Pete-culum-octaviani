8f75344eb0b999c5452e264748b7af61d4d46dd6
HeyJOe03 <jocarmi03@gmail.com>
2021-09-18 10:24:42 +0200

    fetch function

--- SERVER/CLIENT/SCRIPT/busses.js
+++ SERVER/CLIENT/SCRIPT/busses.js
@@ -1,11 +1,19 @@
 
 function loadBusses(){
     const section = document.getElementById('bussesCards')
+    //console.log(window.location.href + '/activeBusses')
+    const options = {
+        method:'POST',
+        body:JSON.stringify({'hello':'i\'m the client'}),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    }
 
-    
+    let address = (window.location.protocol+'//'+window.location.host) 
 
-
-
-
-
+    fetch(address + '/activeBusses',options)
+        .then(result => 
+            result.json()
+                .then((j) => console.log(j))
+        )
+        .catch(err => console.log(err))
 }
